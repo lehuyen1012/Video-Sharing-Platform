6819559721f401590c1adac51007e61fac934aa7
lehuyen1012 <thanhhuyen101203@gmail.com>
2024-09-12 19:25:39 +0700

    First commit - Set up

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl font-pblack">Dofy!</Text>
+            <StatusBar style="auto" />
+            <Link href="/profile" style={{ color: "blue" }}>
+                Go to profile
+            </Link>
+        </View>
+    );
+}
---
+++ app/profile.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const profile = () => {
+    return (
+        <View>
+            <Text>profile</Text>
+        </View>
+    );
+};
+
+export default profile;
+
+const styles = StyleSheet.create({});
