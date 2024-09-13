ebd5c3243adc75b87f0117485fe9fc6ed82d9e20
lehuyen1012 <thanhhuyen101203@gmail.com>
2024-09-13 15:44:39 +0700

    Onboarding Screen

---
+++ components/CustomButton.jsx
@@ -0,0 +1,28 @@
+import { TouchableOpacity, Text } from "react-native";
+import React from "react";
+const CustomButton = ({
+    title,
+    handlePress,
+    containerStyles,
+    textStyles,
+    isLoading,
+}) => {
+    return (
+        <TouchableOpacity
+            onPress={handlePress}
+            activeOpacity={0.8}
+            className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+                isLoading ? "opacity-50" : ""
+            }`}
+            disabled={isLoading}
+        >
+            <Text
+                className={`text-primary font-psemibold text-lg ${textStyles}`}
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+export default CustomButton;
